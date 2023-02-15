bb34ab1870d68b6fb9e799693866da5366ee25ce
SamSepiol <medinadiego29@icloud.com>
2023-02-15 20:23:38 +0100

    Ex1 - Tema 3 PHP Basic- Nivel -2

--- Nivel2/Ex1_Tema3.php
+++ Nivel2/Ex1_Tema3.php
@@ -1,4 +1,13 @@
 <?php 
   //Escriure un programa PHP per calcular la suma dels dos valors enters donats. Si els dos valors són iguals, torna el doble de la seva suma.
+    $x = 3;
+    $y = 3;
+    
+    if($x == $y){
+        $doble = ($x + $y) * 2; 
+        echo $doble;
+    }else{
+        echo $x+$y ;
+    }
 
 ?>
